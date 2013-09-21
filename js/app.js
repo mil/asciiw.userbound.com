280c6bd38277e18729b0ed60980850708a1fe799
mil <miles.sandlar@gmail.com>
2013-09-21 15:42:16 -0400

    some more changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var width  = 100, height = 100;
+var api_key = "dbe04f1b8655b92d";
+var default_zipcode = 24060;
 
 function two_dimensional_array_to_string(array) {
   var ret = "";
@@ -13,6 +14,7 @@ function two_dimensional_array_to_string(array) {
 
 function display_weather(json) {
   console.log(json);
+
   var x = 0;
   setInterval(function() {
     $("body textarea").val(
@@ -20,19 +22,11 @@ function display_weather(json) {
     );
     if (x == 0) { x = 1; } else { x = 0 }
   }, 200);
-
-
 }
 
 function get_weather(zip_code) {
-  $.getJSON("http://api.wunderground.com/api/dbe04f1b8655b92d/conditions/q/" + 24060 + ".json", function(data) {
+  $.getJSON("http://api.wunderground.com/api/" + api_key + "/conditions/q/" + zip_code + ".json", function(data) {
     display_weather(data);
   });
-
-  //setInterval(function() { animate_frame(); }, 1000);
-
-  //weather("rain");
-  //*/
-  
 }
-//window.onload = function() { };
+window.onload = function() { };
